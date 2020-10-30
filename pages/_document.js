ba89ef3c6318c6ccce97c0d4059dec776765b548
Josiah Arkson <34874253+josiaharkson@users.noreply.github.com>
2020-10-30 11:59:58 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,10 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
 
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/a.png" />
+         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+   
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400&display=swap"
