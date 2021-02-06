1336a829b42b0829dbdac0646fab03b7217ba3bc
Josiah Arkson <34874253+josiaharkson@users.noreply.github.com>
2021-02-06 20:16:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const next = require("next");
 const port = parseInt(process.env.PORT, 10) || 5000;
@@ -11,15 +12,16 @@ const mongoose = require("mongoose");
 const { serverRuntimeConfig } = require("next/config").default();
 const MONGO_URI = serverRuntimeConfig.MONGO_URI;
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
 nextApp.prepare().then(() => {
   app.use(express.json());
+  app.use(cors());
+
+  const server = require("http").createServer(app);
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+    },
+  });
 
   mongoose.connect(
     MONGO_URI,
@@ -68,3 +70,48 @@ nextApp.prepare().then(() => {
     console.log(`Listening on port ${PORT}`);
   });
 });
+
+// const express = require("express");
+// const next = require("next");
+// const port = parseInt(process.env.PORT, 10) || 5000;
+// const dev = process.env.NODE_ENV !== "production";
+// const nextApp = next({ dev });
+// const handle = nextApp.getRequestHandler();
+// const mongoose = require("mongoose");
+
+// const { serverRuntimeConfig } = require("next/config").default();
+// const MONGO_URI = serverRuntimeConfig.MONGO_URI;
+
+// nextApp.prepare().then(() => {
+//   const app = express();
+//   app.use(express.json());
+
+//   mongoose.connect(
+//     MONGO_URI,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     },
+//     () => console.log("DB connected")
+//   );
+
+//   // User Routes Endpoint
+//   app.use("/api/user", require("./server/routes/user"));
+
+//   // Country Routes Endpoint
+//   app.use("/api/country", require("./server/routes/country"));
+
+//   // Assist Routes Endpoint
+//   app.use("/api/assist", require("./server/routes/assist"));
+
+//   // Lets next js handle any other route e.g client-side
+//   app.all("*", (req, res) => {
+//     return handle(req, res);
+//   });
+
+//   app.listen(port, err => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
